1170cd0aa0f08c1042b337cbb21082f6bedd1d75
keyonnaise <su.smisc14@gmail.com>
2025-04-06 02:46:40 +0900

    ✨ shallowEquals 함수 생성

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,24 @@
+import { isObject } from "../utils";
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return objA.every((current, i) => current === objB[i]);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const objAKeys = Object.keys(objA) as (keyof typeof objA)[];
+    const objBKeys = Object.keys(objB) as (keyof typeof objB)[];
+
+    if (objAKeys.length !== objBKeys.length) {
+      return false;
+    }
+
+    return objAKeys.every((key) => objA[key] === objB[key]);
+  }
+
+  return false;
 }
