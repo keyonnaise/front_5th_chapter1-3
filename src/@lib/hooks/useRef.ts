5fb716f48fc5913b579f231462e276aad21a8d93
keyonnaise <su.smisc14@gmail.com>
2025-04-06 02:47:14 +0900

    ✨ useRef 훅 생성

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,10 @@
-export function useRef<T>(initialValue: T): { current: T } {
+import { MutableRefObject, useState } from "react";
+
+export function useRef<T = undefined>(): MutableRefObject<T | undefined>;
+export function useRef<T>(initialValue: T): MutableRefObject<T>;
+export function useRef<T>(
+  initialValue?: T,
+): MutableRefObject<T | undefined> | MutableRefObject<T> {
   // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  return useState(() => ({ current: initialValue }))[0];
 }
