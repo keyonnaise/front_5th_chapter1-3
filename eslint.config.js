7b140648ec7164bfc9ccb23538e0fa29c7579f20
keyonnaise <su.smisc14@gmail.com>
2025-04-06 01:37:08 +0900

    🔧 eslint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -21,12 +19,11 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
-  },
-  eslintPluginPrettier,
-  eslintConfigPrettier,
+  }
 );
