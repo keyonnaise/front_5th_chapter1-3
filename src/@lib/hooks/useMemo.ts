6134ce137cad8e51fda3d02d4da14e763051125b
keyonnaise <su.smisc14@gmail.com>
2025-04-06 02:47:39 +0900

    ✨ useMemo 훅 생성

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
@@ -8,5 +8,14 @@ export function useMemo<T>(
   _equals = shallowEquals,
 ): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const memorized = useRef<{ value: T; deps: DependencyList }>();
+
+  if (!memorized.current || !_equals(_deps, memorized.current.deps)) {
+    memorized.current = {
+      value: factory(),
+      deps: _deps,
+    };
+  }
+
+  return memorized.current.value;
 }
