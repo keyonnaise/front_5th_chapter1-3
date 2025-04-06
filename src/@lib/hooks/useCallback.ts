85b0dfb0d8957bf2c4f79f9d1c3b92752e49c0df
keyonnaise <su.smisc14@gmail.com>
2025-04-06 12:45:47 +0900

    ✨ useCallback 훅 생성

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
-import { DependencyList } from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { DependencyList, useMemo } from "react";
 
 export function useCallback<T extends Function>(
   factory: T,
-  _deps: DependencyList,
+  _deps: DependencyList
 ) {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  const memorizedFn = useMemo(() => () => factory(), [factory, ..._deps]);
+  return memorizedFn;
 }
