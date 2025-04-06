4c19f92a4fdfe0e4810f54aec061dc27ce751b18
keyonnaise <su.smisc14@gmail.com>
2025-04-07 03:34:30 +0900

    🚑 useCallback 훅 로직 수정

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -4,13 +4,10 @@ import { DependencyList } from "react";
 import { useRef } from "./useRef";
 import { useMemo } from "./useMemo";
 
-export function useCallback<T extends Function>(
-  factory: T,
-  _deps: DependencyList
-) {
+export function useCallback<T extends Function>(factory: T, _deps: DependencyList) {
   const factoryRef = useRef(factory);
 
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  const memorizedFn = useMemo(() => () => factoryRef.current(), _deps);
+  const memorizedFn = useMemo(() => factoryRef.current, _deps);
   return memorizedFn;
 }
