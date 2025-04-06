2140e23d2609a6f0b3aa593d79fa6696eda7499c
keyonnaise <su.smisc14@gmail.com>
2025-04-06 12:46:42 +0900

    ✨ deepEquals 함수 생성

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,40 @@
+import { isObject } from "../utils";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((current, i) => {
+      if (typeof current === "object") {
+        return deepEquals(current, objB[i]);
+      }
+
+      return current === objB[i];
+    });
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const objAKeys = Object.keys(objA) as (keyof typeof objA)[];
+    const objBKeys = Object.keys(objB) as (keyof typeof objB)[];
+
+    if (objAKeys.length !== objBKeys.length) {
+      return false;
+    }
+
+    return objAKeys.every((key) => {
+      if (typeof objA[key] === "object") {
+        return deepEquals(objA[key], objB[key]);
+      }
+
+      return objA[key] === objB[key];
+    });
+  }
+
+  return false;
 }
