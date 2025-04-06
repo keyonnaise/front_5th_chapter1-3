11f503070038143d79489ac4cb573de4cc3e888e
keyonnaise <su.smisc14@gmail.com>
2025-04-07 03:01:24 +0900

    ✨ AccountProvider 컴포넌트 및 사용자 상태 관리 컨텍스트 추가

---
+++ src/@context/AccountProvider.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { createSafeContext } from "./createSafeContext";
+import { useMemo } from "../@lib/hooks";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function AccountProvider({ children }: Props) {
+  const [state, setState] = useState<ContextState>({
+    user: null,
+  });
+
+  const actions = useMemo<ContextActions>(
+    () => ({
+      login(email) {
+        const DUMMY_USER = {
+          email,
+          id: 1,
+          name: "홍길동",
+        };
+
+        setState((prev) => ({
+          ...prev,
+          user: DUMMY_USER,
+        }));
+      },
+
+      logout() {
+        setState((prev) => ({
+          ...prev,
+          user: null,
+        }));
+      },
+    }),
+    []
+  );
+
+  return (
+    <AccountStateProvider {...state}>
+      <AccountActionsProvider {...actions}>{children}</AccountActionsProvider>
+    </AccountStateProvider>
+  );
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * Types
+ * -----------------------------------------------------------------------------------------------*/
+
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * Context API
+ * -----------------------------------------------------------------------------------------------*/
+
+interface ContextState {
+  user: IUser | null;
+}
+
+interface ContextActions {
+  login(email: string, password: string): void;
+  logout(): void;
+}
+
+export const [AccountStateProvider, useAccountStateProvider] =
+  createSafeContext<ContextState>("AccountProvider");
+export const [AccountActionsProvider, useAccountActionsProvider] =
+  createSafeContext<ContextActions>("AccountProvider");
+
+export default AccountProvider;
